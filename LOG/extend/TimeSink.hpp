9b9197ac9e398129f22ab5a4f443099ddbf0482a
tjq <2780888910@qq.com>
2024-06-21 21:04:08 +0800

    添加注释

--- LOG/extend/TimeSink.hpp
+++ LOG/extend/TimeSink.hpp
@@ -9,91 +9,99 @@
 
 #include "../logs/Sink.hpp"
 
-enum class TimeGap
+namespace ext
 {
-    GAP_SECOND,
-    GAP_MINUTE,
-    GAP_HOUR,
-    GAY_DAY
-};
-
-class RollByTimeSink : public tjq::LogSink
-{
-public:
-    // 构造时传入文件名, 并打开文件, 将操作句柄管理起来
-    RollByTimeSink(const std::string &basename, TimeGap gap_type)
-        : _basename(basename)
+    enum class TimeGap
     {
-        switch (gap_type)
-        {
-        case TimeGap::GAP_SECOND:
-            _gap_size = 1;
-            break;
-        case TimeGap::GAP_MINUTE:
-            _gap_size = 60;
-            break;
-        case TimeGap::GAP_HOUR:
-            _gap_size = 3600;
-            break;
-        case TimeGap::GAY_DAY:
-            _gap_size = 3600 * 24;
-            break;
-        default:
-            std::cerr << "RollByTimeSink: 无效的时间间隔类型" << std::endl;
-        }
-        _cur_gap = getCurGap(); // 获取当前时间段
-        std::string filename = createNewFile();
-        tjq::tool::File::createDirectory(tjq::tool::File::path(filename));
-        _ofs.open(filename, std::ios::binary | std::ios::app);
-        assert(_ofs.is_open());
-    }
+        GAP_SECOND,
+        GAP_MINUTE,
+        GAP_HOUR,
+        GAY_DAY
+    };
 
-    // 将日志消息写入到指定文件, 判断当前时间是否是当前文件的时间段, 不是则切换文件
-    void log(const char *data, size_t len) override
+    /*  扩展一个以时间作为日志文件滚动切换类型的日志落地模块
+        RollByTimeSink(const std::string &basename, TimeGap gap_type);
+        basename: 基础文件名
+        gap_type: 间隔的时间段(时/分/秒/天)
+    */
+    class RollByTimeSink : public tjq::LogSink
     {
-        size_t new_cur = getCurGap();
-        if (new_cur != _cur_gap)
+    public:
+        // 构造时传入文件名, 并打开文件, 将操作句柄管理起来
+        RollByTimeSink(const std::string &basename, TimeGap gap_type)
+            : _basename(basename)
         {
-            _cur_gap = new_cur; // 更新_cur_gap
-            _ofs.close();
+            switch (gap_type)
+            {
+            case TimeGap::GAP_SECOND:
+                _gap_size = 1;
+                break;
+            case TimeGap::GAP_MINUTE:
+                _gap_size = 60;
+                break;
+            case TimeGap::GAP_HOUR:
+                _gap_size = 3600;
+                break;
+            case TimeGap::GAY_DAY:
+                _gap_size = 3600 * 24;
+                break;
+            default:
+                std::cerr << "RollByTimeSink: 无效的时间间隔类型" << std::endl;
+            }
+            _cur_gap = getCurGap(); // 获取当前时间段
             std::string filename = createNewFile();
+            tjq::tool::File::createDirectory(tjq::tool::File::path(filename));
             _ofs.open(filename, std::ios::binary | std::ios::app);
             assert(_ofs.is_open());
         }
-        _ofs.write(data, len);
-        assert(_ofs.good());
-    }
 
-private:
-    // 获取当前时间段
-    size_t getCurGap()
-    {
-        time_t now = tjq::tool::Date::now();
-        return now / _gap_size;
-    }
+        // 将日志消息写入到指定文件, 判断当前时间是否是当前文件的时间段, 不是则切换文件
+        void log(const char *data, size_t len) override
+        {
+            size_t new_cur = getCurGap();
+            if (new_cur != _cur_gap)
+            {
+                _cur_gap = new_cur; // 更新_cur_gap
+                _ofs.close();
+                std::string filename = createNewFile();
+                _ofs.open(filename, std::ios::binary | std::ios::app);
+                assert(_ofs.is_open());
+            }
+            _ofs.write(data, len);
+            assert(_ofs.good());
+        }
 
-    std::string createNewFile()
-    {
-        time_t t = tjq::tool::Date::now();
-        struct tm lt;
-        localtime_r(&t, &lt);
-        std::stringstream filename;
-        filename << _basename;
-        filename << lt.tm_year + 1900;
-        filename << lt.tm_mon + 1;
-        filename << lt.tm_mday;
-        filename << lt.tm_hour;
-        filename << lt.tm_min;
-        filename << lt.tm_sec;
-        filename << ".log";
-        return filename.str();
-    }
+    private:
+        // 获取当前时间段
+        size_t getCurGap()
+        {
+            time_t now = tjq::tool::Date::now();
+            return now / _gap_size;
+        }
 
-private:
-    std::string _basename;
-    std::ofstream _ofs;
-    size_t _cur_gap;  // 当前是第几个时间段
-    size_t _gap_size; // 时间段的大小
-};
+        std::string createNewFile()
+        {
+            time_t t = tjq::tool::Date::now();
+            struct tm lt;
+            localtime_r(&t, &lt);
+            std::stringstream filename;
+            filename << _basename;
+            filename << lt.tm_year + 1900;
+            filename << lt.tm_mon + 1;
+            filename << lt.tm_mday;
+            filename << lt.tm_hour;
+            filename << lt.tm_min;
+            filename << lt.tm_sec;
+            filename << ".log";
+            return filename.str();
+        }
+
+    private:
+        std::string _basename;
+        std::ofstream _ofs;
+        size_t _cur_gap;  // 当前是第几个时间段
+        size_t _gap_size; // 时间段的大小
+    };
+}
 
 #endif

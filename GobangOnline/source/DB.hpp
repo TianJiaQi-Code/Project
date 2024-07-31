ceea330db0aabf417c8cc4b5048b6716a9df8cef
tjq <2780888910@qq.com>
2024-07-31 17:56:12 +0800

    完成数据管理模块设计

--- GobangOnline/source/DB.hpp
+++ GobangOnline/source/DB.hpp
@@ -1,9 +1,17 @@
 #ifndef __G_DB_H__
 #define __G_DB_H__
 
-#include "Util.hpp"
-
 #define INSERT_USER "insert user values(null, '%s', password('%s'), 1000, 0, 0);"
+// 以用户名和密码共同作为查询过滤条件, 查询到数据则表示用户名密码一致, 没有信息则用户名密码错误
+#define LOGIN_USER "select id, score, total_count, win_count from user where username='%s' and password=password('%s');"
+#define USER_BY_NAME "select id, score, total_count, win_count from user where username='%s';"
+#define USER_BY_ID "select username, score, total_count, win_count from user where id=%ld;"
+#define USER_WIN "update user set score=score+30, total_count=total_count+1, win_count=win_count+1 where id=%ld;"
+#define USER_LOSE "update user set score=score-30, total_count=total_count+1 where id=%ld;"
+
+#include <mutex>
+#include <cassert>
+#include "Util.hpp"
 
 class user_table
 {
@@ -18,41 +26,177 @@ public:
         _mysql = mysql_util::mysql_create(host, username, password, dbname, port);
         assert(_mysql != NULL);
     }
+
     ~user_table()
     {
         mysql_util::mysql_destroy(_mysql);
         _mysql = NULL;
     }
+
     // 注册时新增用户
     bool insert(Json::Value &user)
     {
-        Json::Value val;
-        bool ret = select_by_name(user["username"].asCString(), val);
-        if (ret == true)
+        if (user["password"].isNull() || user["username"].isNull())
         {
-            DEBUG("user: %s is already exists", user["username"].asCString());
+            DEBUG("input password or username!");
             return false;
         }
         char sql[4096] = {0};
         sprintf(sql, INSERT_USER, user["username"].asCString(), user["password"].asCString());
-        ret = mysql_util::mysql_exec(_mysql, sql);
+        bool ret = mysql_util::mysql_exec(_mysql, sql);
         if (ret == false)
         {
-            ERROR("insert user info failed!");
+            DEBUG("insert user info failed!");
             return false;
         }
         return true;
     }
+
     // 登录验证, 并返回详细的用户信息
-    bool login(Json::Value &user);
+    bool login(Json::Value &user)
+    {
+        if (user["password"].isNull() || user["username"].isNull())
+        {
+            DEBUG("input password or username!");
+            return false;
+        }
+        char sql[4096] = {0};
+        sprintf(sql, LOGIN_USER, user["username"].asCString(), user["password"].asCString());
+        MYSQL_RES *res = NULL;
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            bool ret = mysql_util::mysql_exec(_mysql, sql);
+            if (ret == false)
+            {
+                DEBUG("user login failed!");
+                return false;
+            }
+            // 要么有数据, 要么没有数据, 就算有数据也只能有一条数据
+            res = mysql_store_result(_mysql);
+            if (res == NULL)
+            {
+                DEBUG("have no login user info!");
+                return false;
+            }
+        }
+        int row_num = mysql_num_rows(res);
+        if (row_num != 1)
+        {
+            DEBUG("the user information queried is not unique!");
+            return false;
+        }
+        MYSQL_ROW row = mysql_fetch_row(res);
+        user["id"] = (Json::UInt64)std::stol(row[0]);
+        user["score"] = (Json::UInt64)std::stol(row[1]);
+        user["total_count"] = std::stoi(row[2]);
+        user["win_count"] = std::stoi(row[3]);
+        mysql_free_result(res);
+        return true;
+    }
+
     // 通过用户名获取用户信息
-    bool select_by_name(const std::string &name, Json::Value &user);
+    bool select_by_name(const std::string &name, Json::Value &user)
+    {
+        char sql[4096] = {0};
+        sprintf(sql, USER_BY_NAME, name.c_str());
+        MYSQL_RES *res = NULL;
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            bool ret = mysql_util::mysql_exec(_mysql, sql);
+            if (ret == false)
+            {
+                DEBUG("get user by name failed!");
+                return false;
+            }
+            // 要么有数据, 要么没有数据, 就算有数据也只能有一条数据
+            res = mysql_store_result(_mysql);
+            if (res == NULL)
+            {
+                DEBUG("have no user info!");
+                return false;
+            }
+        }
+        int row_num = mysql_num_rows(res);
+        if (row_num != 1)
+        {
+            DEBUG("the user information queried is not unique!");
+            return false;
+        }
+        MYSQL_ROW row = mysql_fetch_row(res);
+        user["id"] = (Json::UInt64)std::stol(row[0]);
+        user["username"] = name;
+        user["score"] = (Json::UInt64)std::stol(row[1]);
+        user["total_count"] = std::stoi(row[2]);
+        user["win_count"] = std::stoi(row[3]);
+        mysql_free_result(res);
+        return true;
+    }
+
     // 通过id获取用户信息
-    bool select_by_id(int id, Json::Value &user);
-    // 胜利时天梯分数增加, 战斗场次增加, 胜利场次增加
-    bool win(int id);
-    // 失败时天梯分数减少, 战斗场次增加, 其他不变
-    bool lose(int id);
+    bool select_by_id(uint64_t id, Json::Value &user)
+    {
+        char sql[4096] = {0};
+        sprintf(sql, USER_BY_ID, id);
+        MYSQL_RES *res = NULL;
+        {
+            std::unique_lock<std::mutex> lock(_mutex);
+            bool ret = mysql_util::mysql_exec(_mysql, sql);
+            if (ret == false)
+            {
+                DEBUG("get user by id failed!");
+                return false;
+            }
+            // 要么有数据, 要么没有数据, 就算有数据也只能有一条数据
+            res = mysql_store_result(_mysql);
+            if (res == NULL)
+            {
+                DEBUG("have no user info!");
+                return false;
+            }
+        }
+        int row_num = mysql_num_rows(res);
+        if (row_num != 1)
+        {
+            DEBUG("the user information queried is not unique!");
+            return false;
+        }
+        MYSQL_ROW row = mysql_fetch_row(res);
+        user["id"] = (Json::UInt64)id;
+        user["username"] = row[0];
+        user["score"] = (Json::UInt64)std::stol(row[1]);
+        user["total_count"] = std::stoi(row[2]);
+        user["win_count"] = std::stoi(row[3]);
+        mysql_free_result(res);
+        return true;
+    }
+
+    // 胜利时天梯分数增加30, 战斗场次增加1, 胜利场次增加1
+    bool win(uint64_t id)
+    {
+        char sql[4096] = {0};
+        sprintf(sql, USER_WIN, id);
+        bool ret = mysql_util::mysql_exec(_mysql, sql);
+        if (ret == false)
+        {
+            DEBUG("update win user info failed!");
+            return false;
+        }
+        return true;
+    }
+
+    // 失败时天梯分数减少30, 战斗场次增加1, 其他不变
+    bool lose(uint64_t id)
+    {
+        char sql[4096] = {0};
+        sprintf(sql, USER_LOSE, id);
+        bool ret = mysql_util::mysql_exec(_mysql, sql);
+        if (ret == false)
+        {
+            DEBUG("update lose user info failed!");
+            return false;
+        }
+        return true;
+    }
 
 private:
     MYSQL *_mysql;     // mysql操作句柄
--- GobangOnline/source/DB.sql
+++ GobangOnline/source/DB.sql
@@ -1,11 +1,12 @@
 -- 数据管理模块
 -- 1. 数据库表的设计
+drop database if exists gobang;
 create database if not exists gobang;
 use gobang;
 create table if not exists user(
     id int primary key auto_increment,
-    username varchar(32) not null,
-    password varchar(32) not null,
+    username varchar(32) unique key not null,
+    password varchar(128) not null,
     score int,
     total_count int,
     win_count int

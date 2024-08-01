39ac3a32ef2ce7b98a27bb29b8e651b0178c4ad9
tjq <2780888910@qq.com>
2024-08-01 23:20:11 +0800

    修改日志宏名称 避免重复定义

--- GobangOnline/logs/Log.h
+++ GobangOnline/logs/Log.h
@@ -8,22 +8,22 @@
 
 #include "Logger.hpp"
 
-namespace tjq
-{
 // 使用宏函数对日志器的接口进行代理(代理模式)
-#define debug(fmt, ...) debug(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define info(fmt, ...) info(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define warn(fmt, ...) warn(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define error(fmt, ...) error(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
-#define fatal(fmt, ...) fatal(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define log_debug(fmt, ...) debug(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define log_info(fmt, ...) info(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define log_warn(fmt, ...) warn(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define log_error(fmt, ...) error(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+#define log_fatal(fmt, ...) fatal(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
 
 // 提供宏函数, 直接通过默认日志器进行日志的标准输出打印(不用获取日志器了)
-#define DEBUG(fmt, ...) tjq::rootLogger()->debug(fmt, ##__VA_ARGS__)
-#define INFO(fmt, ...) tjq::rootLogger()->info(fmt, ##__VA_ARGS__)
-#define WARN(fmt, ...) tjq::rootLogger()->warn(fmt, ##__VA_ARGS__)
-#define ERROR(fmt, ...) tjq::rootLogger()->error(fmt, ##__VA_ARGS__)
-#define FATAL(fmt, ...) tjq::rootLogger()->fatal(fmt, ##__VA_ARGS__)
+#define DEBUG(fmt, ...) tjq::rootLogger()->log_debug(fmt, ##__VA_ARGS__)
+#define INFO(fmt, ...) tjq::rootLogger()->log_info(fmt, ##__VA_ARGS__)
+#define WARN(fmt, ...) tjq::rootLogger()->log_warn(fmt, ##__VA_ARGS__)
+#define ERROR(fmt, ...) tjq::rootLogger()->log_error(fmt, ##__VA_ARGS__)
+#define FATAL(fmt, ...) tjq::rootLogger()->log_fatal(fmt, ##__VA_ARGS__)
 
+namespace tjq
+{
     // 提供获取指定日志器的全局接口(避免用户自己操作单例对象)
     Logger::ptr getLogger(const std::string &name)
     {
